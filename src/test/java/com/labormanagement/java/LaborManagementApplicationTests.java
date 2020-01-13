82cde13416494bdbea36c29eececd7e0621f611f
LiaoSY <shiyaoliao@yahoo.com>
2020-01-12 19:52:02 -0500

    First initialization.

---
+++ src/test/java/com/labormanagement/java/LaborManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labormanagement.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaborManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
