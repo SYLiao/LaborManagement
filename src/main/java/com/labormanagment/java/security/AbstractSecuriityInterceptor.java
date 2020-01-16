0945089020c9f5ed7c4ebaf6d73fc1a6ab395f94
Jordan I <eblazer.eblazer@gmail.com>
2020-01-16 11:34:52 -0500

    Add files via upload

--- src/main/java/com/labormanagment/java/security/AbstractSecuriityInterceptor.java
+++ src/main/java/com/labormanagment/java/security/AbstractSecuriityInterceptor.java
@@ -2,21 +2,7 @@ package com.labormanagment.java.security;
 
 public class AbstractSecuriityInterceptor {
 	
-	/*
-	 * Copyright 2004, 2005, 2006 Acegi Technology Pty Limited
-	 *
-	 * Licensed under the Apache License, Version 2.0 (the "License");
-	 * you may not use this file except in compliance with the License.
-	 * You may obtain a copy of the License at
-	 *
-	 *      https://www.apache.org/licenses/LICENSE-2.0
-	 *
-	 * Unless required by applicable law or agreed to in writing, software
-	 * distributed under the License is distributed on an "AS IS" BASIS,
-	 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	 * See the License for the specific language governing permissions and
-	 * limitations under the License.
-	 */
+
 
 	package org.springframework.security.access.intercept;
 
@@ -51,7 +37,7 @@ public class AbstractSecuriityInterceptor {
 	import org.springframework.security.core.context.SecurityContextHolder;
 	import org.springframework.util.Assert;
 
-	/**
+	/
 	 * Abstract class that implements security interception for secure objects.
 	
 	public abstract class AbstractSecurityInterceptor implements InitializingBean,
