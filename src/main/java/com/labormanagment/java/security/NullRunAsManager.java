c8957277c165921710b421bb4cd65d7814a2ce58
Jordan I <eblazer.eblazer@gmail.com>
2020-01-16 14:10:46 -0500

    Add files via upload

---
+++ src/main/java/com/labormanagment/java/security/NullRunAsManager.java
@@ -0,0 +1,25 @@
+package com.labormanagment.java.security;
+
+import java.util.Collection;
+
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.access.intercept.RunAsManager;
+import org.springframework.security.core.Authentication;
+
+final class NullRunAsManager implements RunAsManager {
+	// ~ Methods
+	// ========================================================================================================
+
+	public Authentication buildRunAs(Authentication authentication, Object object,
+			Collection<ConfigAttribute> config) {
+		return null;
+	}
+
+	public boolean supports(ConfigAttribute attribute) {
+		return false;
+	}
+
+	public boolean supports(Class<?> attribute) {
+		return true;
+	}
+}
